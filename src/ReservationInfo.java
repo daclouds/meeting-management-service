2451fb04885ca790a03870c33e3a3e55bb791783
1002482 <1002482@SKP1002482MN001.local>
2015-02-16 13:34:51 +0900

    class 작성

---
+++ src/ReservationInfo.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+
+public class ReservationInfo {
+	public Date startTime;			// 시작시간
+	public Date endTime;			// 끝시간
+	public String departmentName;  // 부서명
+	public String reserver;		// 예약자
+	public String contactNumber;	// 연락처
+	public String meetingTitle;	// 회의제목
+    
+	public MeetingRoom meetingRoom;
+    
+    
+}
