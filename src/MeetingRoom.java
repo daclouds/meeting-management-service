dd318bd59e827bedd609d2eb9bbed2724477d041
1002473 <1002473@SKP1002473MN001.local>
2015-02-16 13:34:48 +0900

    first

---
+++ src/MeetingRoom.java
@@ -0,0 +1,13 @@
+public class MeetingRoom{
+	  String roomNumber;
+	  int floor;
+	  int capacityNumber;
+	  int equipment;
+	  
+	  public MeetingRoom(){
+		  this.roomNumber=new String();
+		  this.floor=0;
+		  this.capacityNumber=0;
+		  this.equipment= 0x000000;
+	  }
+	}
